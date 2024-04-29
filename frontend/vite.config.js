8e9e2e85d1aa9156342ea8c5861ca19694d7c5bb
Robert Tezock <105029396+tezock@users.noreply.github.com>
2024-04-29 17:31:29 -0500

    updated base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "mythology-chatbot",
+  base: "./Mythology-Chatbot",
   plugins: [
     react(),
   ],
